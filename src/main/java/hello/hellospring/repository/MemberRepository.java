7ddceb01808985560ab3413ec5ac23e2015a0f5e
hanbin8269 <hanbin8269@gmail.com>
2020-12-29 20:14:55 +0900

    Create MemberRepository interface

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    // Optional ?? null을 처리할떄 optional로 감싼다고 함
+    List<Member> findAll();
+}

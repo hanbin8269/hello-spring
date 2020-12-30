b952dd2635253ff15989f0fc4915d4befa39d711
hanbin8269 <hanbin8269@gmail.com>
2020-12-30 23:38:49 +0900

    Fix : Add annotation

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -1,4 +1,4 @@
-package hello.hellospring;
+package hello.hellospring; // 기본적으로 이 패키지 안에 있는 파일만 컴포넌트 스캔
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

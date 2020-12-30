21ae66fbc6d855c31f11164cfa10b3a03e42247d
hanbin8269 <hanbin8269@gmail.com>
2020-12-31 00:12:00 +0900

    Feat : Create join method

--- src/main/java/hello/hellospring/controller/MemberContorller.java
+++ src/main/java/hello/hellospring/controller/MemberContorller.java
@@ -1,8 +1,12 @@
 package hello.hellospring.controller;
 
 import hello.hellospring.service.MemberService;
+import hello.hellospring.domain.Member;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberContorller {
@@ -12,5 +16,20 @@ public class MemberContorller {
     @Autowired // Dependency injection (의존성 주입)
     public MemberContorller(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    @GetMapping("members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+        System.out.println(form.getName());
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
